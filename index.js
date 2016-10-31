344200b7efb44428c77cccedb2413e0ae151f98c
deq2000 <inazu2014@gmail.com>
2016-10-31 22:38:26 +0000

    Managed to complete 2.0

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ $(document).ready(function() {
         var value2 = parseInt($('#myInput2').val());
         console.log(value1);
         console.log(value2);
+        var UB01 = document.getElementById("myInput3");
+        var UB02 = document.getElementById("myInput4");
         if (value2 >= value1) {
             $("#Judgement").html("Enter if you dare.");
         } else {
-            $("#Judgement").html("BEGONE you feeble swine.");
+            if (UB01.checked == true && UB02.checked == true) {
+                $("#Judgement").html("BEGONE you feeble swine.");
+            } else if (UB01.checked == true) {
+                $("#Judgement").html("You lucky plebe...");
+            } else {
+                $("#Judgement").html("BEGONE you feeble swine.");
+            }
         }
     });
 });
